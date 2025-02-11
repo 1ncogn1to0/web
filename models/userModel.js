5a69dcad4e04b7a7b2d2e49bfe9451876190632e
1ncogn1to0 <161634035+1ncogn1to0@users.noreply.github.com>
2025-02-12 00:44:39 +0500

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
 //userModel.js
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
