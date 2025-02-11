221fd4cec03d1e372e56bc0f0259286e4f148eee
1ncogn1to0 <161634035+1ncogn1to0@users.noreply.github.com>
2025-02-11 22:21:12 +0500

    Update app.js

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.post('/api/otp/send', async (req, res) => {
 app.use(express.static(path.join(__dirname, '/public')));
 
 console.log('PORT:', process.env.PORT); // Добавьте это для диагностики
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
