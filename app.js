7e226e3374954271915fc04148ad258e6bbd41ed
1ncogn1to0 <161634035+1ncogn1to0@users.noreply.github.com>
2025-02-11 22:13:46 +0500

    Update app.js

--- app.js
+++ app.js
@@ -44,6 +44,9 @@ app.post('/api/otp/send', async (req, res) => {
 // Подключение папки public
 app.use(express.static(path.join(__dirname, '/public')));
 
+console.log('PORT:', process.env.PORT); // Добавьте это для диагностики
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
 
