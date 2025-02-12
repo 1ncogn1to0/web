e70aaead4b772f3cea072cfe0c7dc66227afe34b
1ncogn1to0 <161634035+1ncogn1to0@users.noreply.github.com>
2025-02-12 12:14:57 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,11 @@
-// config/db.js
+//config/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const dbOptions = {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            
-        };
+        const mongoURI = process.env.DATABASE_URL; // Используем Atlas, если доступен
 
-        const mongoURI = process.env.DATABASE_URL;
-        await mongoose.connect(mongoURI, dbOptions);
+        await mongoose.connect(mongoURI);
 
         console.log(`✅ MongoDB Connected: ${mongoURI.includes('127.0.0.1') ? 'Local DB' : 'Atlas Cloud'}`);
     } catch (err) {
